831717887de2100a454219bfe269a7d03081812d
porporpor020202 <porporpor020202@gmail.com>
2024-07-26 17:54:22 +0900

    feat: NativeWind 추가
    프로젝트에 NAtiveWind를 적용하였습니다

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <View style={styles.view}>
-        <Text style={styles.text}>Hellow World!</Text>
+      <View>
+        <Text className="text-red-500">Hellow World!</Text>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  view: {
-    alignItems: 'center',
-  },
-  text: {
-    color: 'orange',
-  },
-});
 
 export default App;
