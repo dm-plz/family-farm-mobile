4e3d4252904edd97afd766c258613d1c0e10d13f
timepresent95 <timepresent95@gmail.com>
2024-07-31 03:05:12 +0900

    feat: hide icon when iconText is empty in MainHeader component
    #4

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 interface Props {
-  iconText: string;
+  iconText?: string;
 }
 
 export default function MainHeader({iconText}: Props) {
   return (
     <View className="relative mb-10">
       <Text className="text-center text-xl">Logo</Text>
-      <Text className="absolute right-0 mr-4">{iconText}</Text>
+      {iconText && <Text className="absolute right-0 mr-4">{iconText}</Text>}
     </View>
   );
 }
