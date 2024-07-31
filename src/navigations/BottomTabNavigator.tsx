d16289718ff1883eb47782ca0424b050b058b862
timepresent95 <timepresent95@gmail.com>
2024-07-31 19:55:45 +0900

    feat: create navigation menu in bottom tab navigation
    #4

--- src/navigations/BottomTabNavigator.tsx
+++ src/navigations/BottomTabNavigator.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Sett01 from '@/pages/Sett01';
+import Main01 from '@/pages/Main01';
+import Mymy01 from '@/pages/Mymy01';
+import {colors} from '@/constants';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="설정" component={Sett01} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.YELLOW_400,
+        tabBarInactiveTintColor: colors.WHITE,
+        tabBarStyle: {
+          backgroundColor: colors.GREEN_700,
+          minHeight: 80,
+          paddingBottom: 20,
+          paddingTop: 10,
+        },
+      }}>
+      <Tab.Screen
+        name="홈"
+        component={Main01}
+        options={{
+          tabBarIcon: ({color}) => <Text style={{color}}>홈 아이콘</Text>,
+        }}
+      />
+      <Tab.Screen
+        name="질문 답변"
+        component={Mymy01}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Text style={{color}}>질문 답변 아이콘</Text>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="마이"
+        component={Mymy01}
+        options={{
+          tabBarIcon: ({color}) => <Text style={{color}}>마이 아이콘</Text>,
+        }}
+      />
     </Tab.Navigator>
   );
 }
