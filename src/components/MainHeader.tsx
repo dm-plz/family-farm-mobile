2d88684941c7c8e4b683d2362436be412f80e1fd
timepresent95 <timepresent95@gmail.com>
2024-07-31 20:51:40 +0900

    feat: add onPressIcon props at MainHeader Component
    #4

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -3,13 +3,18 @@ import {Text, View} from 'react-native';
 
 interface Props {
   iconText?: string;
+  onPressIcon?: () => void;
 }
 
-export default function MainHeader({iconText}: Props) {
+export default function MainHeader({iconText, onPressIcon = () => {}}: Props) {
   return (
     <View className="relative mb-10">
       <Text className="text-center text-xl">Logo</Text>
-      {iconText && <Text className="absolute right-0 mr-4">{iconText}</Text>}
+      {iconText && (
+        <Text className="absolute right-0 mr-4" onPress={onPressIcon}>
+          {iconText}
+        </Text>
+      )}
     </View>
   );
 }
