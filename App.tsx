776acd0f794a5c7e79781c57157be1056029d1e2
timepresent95 <timepresent95@gmail.com>
2024-08-02 17:20:38 +0900

    build: install @tanstack/react-query, react-native-encrypted-storage
    #19

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import {QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
+import queryClient from '@/api/queryClient';
+
 function App() {
   return (
-    <SafeAreaView>
-      <Text className="text-orange-300">Hellow World</Text>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView>
+        <Text className="text-orange-300">Hellow World</Text>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
