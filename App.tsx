74d93fb94ea15cf7ab11bdee196709df183e8861
timepresent95 <timepresent95@gmail.com>
2024-08-03 23:25:25 +0900

    fix: add missing code when merge with develop branch
    associated commit number: 0bbab4
    
    #19

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 
+import queryClient from '@/api/queryClient';
 import SignUpStackNavigator from '@/navigations/stack/SignUpStackNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SignUpStackNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <SignUpStackNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
