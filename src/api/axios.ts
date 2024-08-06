7b99a08aa5cdaa4eed3332a5f97072233fcb94ce
timepresent95 <timepresent95@gmail.com>
2024-08-06 18:22:24 +0900

    refactor: change the management location of API_BASE_URL
    #19

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,10 @@
+//FIXME: 서버 작업이 완료되면 axios 모듈 사용으로 수정해야 함
 import axios from 'axios';
+import {Config} from 'react-native-config';
+// import axios from '@/mock/mockAxios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.35:3030',
+  baseURL: Config.API_BASE_URL,
   timeout: 5000,
   withCredentials: true,
 });
