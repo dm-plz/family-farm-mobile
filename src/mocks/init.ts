2d1ffc95a9dc6da28654b0859980a35c326f3abb
timepresent95 <timepresent95@gmail.com>
2024-08-10 01:32:56 +0900

    feat: add trigger for loggin mocked response by msw
    #19

--- src/mocks/init.ts
+++ src/mocks/init.ts
@@ -4,5 +4,14 @@ export async function initMSW() {
   }
   await import('./msw.polyfills');
   const {server} = await import('./server');
-  server.listen();
+  server.listen({onUnhandledRequest: 'bypass'});
+  server.events.on('response:mocked', ({request, response}) => {
+    console.log(
+      '%s %s received %s %s',
+      request.method,
+      request.url,
+      response.status,
+      response.statusText,
+    );
+  });
 }
