6c997126f49019b419901590b4484e670e4170e2
timepresent95 <timepresent95@gmail.com>
2024-08-14 01:14:26 +0900

    feat: create getuserFromToken function
    this function extract user info from token
    
    #19

---
+++ src/utils/token.ts
@@ -0,0 +1,22 @@
+import {Member} from '@/types';
+
+//NOTE: 유저 정보를 토큰에서 얻기 위한 함수
+//FIXME: BE에서 전달해준 payload가 정해지면 수정해야 함
+export function getUserFromToken(token: string): Member | null {
+  try {
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+      return null;
+    }
+
+    const [, encodedPayload] = parts;
+    const decodedPayload = Buffer.from(encodedPayload, 'base64').toString(
+      'utf-8',
+    );
+    const payload = JSON.parse(decodedPayload);
+
+    return payload as Member;
+  } catch (error) {
+    return null;
+  }
+}
