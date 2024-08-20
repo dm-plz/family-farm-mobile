f587e53f2aae62e8f3dfffb7cc58fae53b944d42
timepresent95 <timepresent95@gmail.com>
2024-08-20 19:34:58 +0900

    feat: add family API and types
    #19

---
+++ src/api/family.ts
@@ -0,0 +1,38 @@
+import {kyInstance} from './ky';
+import {familyApis} from './routes';
+
+import {createUrl} from '@/utils/url';
+
+type ParamGetFamilyCode = {
+  familyId: string;
+};
+
+type ResponseGetFamilyCode = {
+  code: string;
+};
+
+async function getFamilyCode(param: ParamGetFamilyCode) {
+  const apiUrl = createUrl(familyApis.getFamilyCode, {param});
+  return await kyInstance.get(apiUrl).json<ResponseGetFamilyCode>();
+}
+
+type ParamRefreshFamilyCode = {
+  familyId: string;
+};
+
+type ResponseRefreshFamilyCode = {
+  code: string;
+};
+
+async function postSurvey(param: ParamRefreshFamilyCode) {
+  const apiUrl = createUrl(familyApis.refreshFamilyCode, {param});
+  return await kyInstance.post(apiUrl).json<ResponseRefreshFamilyCode>();
+}
+
+export {getFamilyCode, postSurvey};
+export type {
+  ParamGetFamilyCode,
+  ResponseGetFamilyCode,
+  ParamRefreshFamilyCode,
+  ResponseRefreshFamilyCode,
+};
