ea727c76986d7deab76adbefc0ad39c0c9154267
timepresent95 <timepresent95@gmail.com>
2024-08-24 00:09:33 +0900

    feat: google signin in android
    #20

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,17 +1,39 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { Image, Platform, Pressable, Text, View } from 'react-native';
 
 import { signUpNavigation } from '@/constants';
 import { SignUpStackParamList } from '@/navigations/stack/SignUpStackNavigator';
-
 type OnboardingProps = NativeStackScreenProps<
   SignUpStackParamList,
   typeof signUpNavigation.ONBOARDING
 >;
 
+//NOTE: 추가로 client id를 사용할 일이 생길 경우 별도로 분리 해야 함
+//FIXME: 해당 파일이 아닌 다른 곳에서 로그인 로직을 전체적으로 관리해야 함
+GoogleSignin.configure({
+  webClientId:
+    '686661822188-g09au2cca8vhjbbsq14c2aq5cd54j2h0.apps.googleusercontent.com',
+  offlineAccess: true,
+});
+
 function Onboarding({ navigation }: OnboardingProps) {
   const isIOS = Platform.OS === 'ios';
+
+  const googleSignin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      //NOTE: userInfo 값에서 idToken을 사용하면 됨
+      console.log(userInfo);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <View className="h-full">
@@ -25,6 +47,11 @@ function Onboarding({ navigation }: OnboardingProps) {
       <View className="flex basis-1/3 flex-col px-10">
         <Pressable className="mb-4 w-full bg-yellow-200 px-4 py-6">
           <Text>카카오 로그인</Text>
+        </Pressable>
+        <Pressable
+          className="mb-4 w-full bg-green-200 px-4 py-6"
+          onPress={googleSignin}>
+          <Text>구글 로그인</Text>
         </Pressable>
         <RenderLoginButtonByPlatform isIOS={isIOS} />
 
