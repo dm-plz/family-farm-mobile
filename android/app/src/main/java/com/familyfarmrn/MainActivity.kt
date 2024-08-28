1cbd9073699f62f51cd259d4aa0b217ca442e6d6
timepresent95 <timepresent95@gmail.com>
2024-08-29 03:22:16 +0900

    feat: FCM init setting in android
    #35

--- android/app/src/main/java/com/familyfarmrn/MainActivity.kt
+++ android/app/src/main/java/com/familyfarmrn/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.familyfarmrn
 
+import android.content.pm.PackageManager
+import android.os.Build
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,7 +13,23 @@ import com.familyfarmrn.KakaoLoginModule.Companion.TAG
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessaging
+
 class MainActivity : ReactActivity() {
+
+  private val requestPermissionLauncher = registerForActivityResult(
+    ActivityResultContracts.RequestPermission(),
+  ) { isGranted: Boolean ->
+    if (isGranted) {
+      // FCM SDK (and your app) can post notifications.
+    } else {
+      // TODO: Inform user that your app will not show notifications.
+    }
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -26,10 +44,47 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
+  private fun askNotificationPermission() {
+    // This is only necessary for API level >= 33 (TIRAMISU)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      if (ContextCompat.checkSelfPermission(
+          this,
+          Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
+      ) {
+        // FCM SDK (and your app) can post notifications.
+      } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+        // TODO: display an educational UI explaining to the user the features that will be enabled
+        // by them granting the POST_NOTIFICATION permission. This UI should provide the user
+        // "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+        // If the user selects "No thanks," allow the user to continue without notifications.
+      } else {
+        // Directly ask for the permission
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+      }
+    }
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
 
+    askNotificationPermission()
+
+    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+      if (!task.isSuccessful) {
+        Log.w("MyApp", "Fetching FCM registration token failed", task.exception)
+        return@addOnCompleteListener
+      }
+
+      // Get new FCM registration token
+      val token = task.result
+
+      // Log and toast
+//      val msg = getString(R.string.msg_token_fmt, token)
+      Log.d("MyApp", token)
+//      Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+    }
+
     KakaoSdk.init(this, resources.getString(R.string.kakao_app_key))
-    Log.d(TAG, "key hash : ${Utility.getKeyHash(this)}")
   }
 }
