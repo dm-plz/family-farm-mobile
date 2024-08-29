13c189e9e7b814d7c276ebdb6f1c70042f446bdd
timepresent95 <timepresent95@gmail.com>
2024-08-29 18:23:15 +0900

    feat: FCM init setting in ios
    #35

--- ios/FamilyFarmRN/AppDelegate.mm
+++ ios/FamilyFarmRN/AppDelegate.mm
@@ -1,12 +1,35 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UserNotifications.h>
 #import "KakaoLoginModule.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  
+  [FIRMessaging messaging].delegate = self;
+  
+  // 푸시 알림 권한 요청 및 APNs 등록 코드 추가
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;  // 이제 이 줄이 올바르게 동작합니다.
+
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  
+  [center requestAuthorizationWithOptions:authOptions
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    if (granted) {
+      NSLog(@"Push notification permission granted.");
+    } else {
+      NSLog(@"Push notification permission denied.");
+    }
+  }];
+
+  [application registerForRemoteNotifications];
+  
   self.moduleName = @"FamilyFarmRN";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
@@ -25,6 +48,50 @@
 
  return NO;
 }
+
+// FIRMessagingDelegate 메서드 구현
+// 등록 토큰을 수신할 때 호출되는 메서드
+- (void)messaging:(FIRMessaging *)messaging didReceiveRegistrationToken:(NSString *)fcmToken {
+    NSLog(@"FCM registration token: %@", fcmToken);
+
+    // 토큰을 서버로 전송하거나 저장하는 등의 추가 작업을 수행
+}
+
+// 앱이 포그라운드에 있을 때 푸시 알림을 처리하는 메서드
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    NSDictionary *userInfo = notification.request.content.userInfo;
+
+    // Analytics 연동 (Swizzling이 비활성화된 경우) -> 현재는 활성화 되어있는 상태
+    // [[FIRMessaging messaging] appDidReceiveMessage:userInfo];
+
+    // 알림 내용 로그 출력
+    NSLog(@"%@", userInfo);
+
+    // 사용자에게 어떤 방식으로 알림을 표시할지 결정
+    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionAlert);
+}
+
+// 사용자가 알림을 클릭했을 때 호출되는 메서드
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void(^)(void))completionHandler {
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    
+    if (userInfo[@"gcm.message_id"]) {
+        NSLog(@"Message ID: %@", userInfo[@"gcm.message_id"]);
+    }
+
+    // Analytics 연동 (Swizzling이 비활성화된 경우) -> 현재는 활성화 되어있는 상태
+    // [[FIRMessaging messaging] appDidReceiveMessage:userInfo];
+
+    // 알림 내용 로그 출력
+    NSLog(@"%@", userInfo);
+
+    completionHandler();
+}
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
