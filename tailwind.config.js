9db84d5a447edc2b9a5c2c36292517940a62fd9a
timepresent95 <timepresent95@gmail.com>
2024-09-24 16:14:34 +0900

    feat: add color style theme
    #48

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      gray: {
+        0: '#ffffff',
+        25: '#f7f7f7',
+        50: '#f1f1f1',
+        100: '#e1e1e1',
+        200: '#c8c8c8',
+        300: '#999999',
+        400: '#666666',
+        500: '#222222',
+      },
+      primary: {
+        100: '#1bb876',
+        'gradient-init': '#1bb876',
+        'gradient-end': '#1bb892',
+        8: '#1bb876',
+      },
+      secondary: '#ff2b64',
+      success: '#00a862',
+      error: 'a83c00',
+    },
     extend: {},
   },
   plugins: [],
