f8e59a395a477ddd7cad4145c27d932790158080
timepresent95 <timepresent95@gmail.com>
2024-09-29 23:06:15 +0900

    feat: modify common style elements
    #56

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,23 @@
 const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  gray: {
+    25: '#f7f7f7',
+    50: '#f1f1f1',
+    100: '#e1e1e1',
+    200: '#c8c8c8',
+    300: '#999999',
+    400: '#666666',
+    500: '#222222',
+  },
+  primary: {
+    100: '#1bb876',
+    8: '#1bb876',
+  },
+  secondary: '#ff2b64',
+  success: '#00a862',
+  error: '#a83c00',
+  //XXX: 걷어 내야 함
   BLACK: '#000000',
   WHITE: '#ffffff',
   SLATE_200: '#e2e8f0',
