906aa26d2051129f5f030e784e6dada844abfc76
timepresent95 <timepresent95@gmail.com>
2024-09-30 12:57:11 +0900

    feat: adapt design at setting page
    #56

--- src/pages/my/Setting.tsx
+++ src/pages/my/Setting.tsx
@@ -1,9 +1,17 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
-import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  View,
+} from 'react-native';
 
 import MainHeader from '@/components/my/MainHeader';
-import { myNavigation } from '@/constants';
+import { colors, myNavigation } from '@/constants';
+import { TextBold, TextMedium, TextRegular } from '@/entities/fonts';
 import { MyStackParamList } from '@/navigations/stack/MyStackNavigator';
 
 type SettingScreenProps = NativeStackScreenProps<
@@ -11,6 +19,7 @@ type SettingScreenProps = NativeStackScreenProps<
   typeof myNavigation.SETTING
 >;
 export default function Setting({ navigation }: SettingScreenProps) {
+  // TODO: Modal Page 구현 후 적용
   const showAlert = () => {
     Alert.alert(
       '로그아웃 확인',
@@ -25,54 +34,140 @@ export default function Setting({ navigation }: SettingScreenProps) {
       { cancelable: false },
     );
   };
+  const [isAlarm, setIsAlram] = useState(false);
+  const [isMarketing, setIsMarketing] = useState(false);
 
   return (
-    <SafeAreaView className="h-full bg-green-100">
-      <View>
-        <MainHeader />
-        <View className="px-4">
-          <View>
-            <Text onPress={() => navigation.navigate(myNavigation.MY)}>
-              뒤로가기 버튼
-            </Text>
-          </View>
-          <View className="mt-6 px-4">
-            <View className="mt-4 space-y-8">
-              <Text className="text-xl font-bold">알림 설정</Text>
-              <View className="flex flex-row justify-between">
-                <Text>PUSH 알림</Text>
-                <Text>PUSH 알림 상태 표시</Text>
-              </View>
-              <View className="flex flex-row justify-between">
-                <Text>마케팅 정보 알림</Text>
-                <Text>마케팅 정보 알림 상태 표시</Text>
-              </View>
-            </View>
-            <View className="mt-4 space-y-8">
-              <Text className="text-xl font-bold">문의하기</Text>
-              <View className="flex flex-row justify-between">
-                <Text>자주 묻는 질문</Text>
-                <Text>자주 묻는 질문 이동 버튼</Text>
-              </View>
-              <View className="flex flex-row justify-between">
-                <Text>1:1 문의</Text>
-                <Text>1:1 문의 이동 버튼</Text>
-              </View>
-            </View>
-            <View className="mt-4 space-y-8">
-              <Text className="text-xl font-bold">개인/보안</Text>
-              <View className="flex flex-row justify-between">
-                <Text>로그아웃</Text>
-                <Button title="로그아웃 이동 버튼" onPress={showAlert} />
-              </View>
-              <View className="flex flex-row justify-between">
-                <Text>탈퇴하기</Text>
-                <Text>탈퇴하기 이동 버튼</Text>
-              </View>
-            </View>
-          </View>
+    <SafeAreaView className="bg-white h-full">
+      <MainHeader
+        left={{
+          onPress: () => navigation.goBack(),
+          icon: (
+            <Image
+              source={require('@/assets/img/icon-arrow-left.png')}
+              resizeMode="contain"
+              className="h-5 w-5"
+              tintColor={colors.primary[100]}
+            />
+          ),
+        }}
+        title="설정"
+      />
+      <View className="mx-5 my-5 flex flex-row items-center">
+        <Image
+          source={require('@/assets/img/default-user-profile.png')}
+          className="mr-2 h-12 w-12"
+          resizeMode="contain"
+        />
+        <View>
+          <TextBold className="text-h3">가은님</TextBold>
         </View>
+        <Image
+          source={require('@/assets/img/icon-arrow-left.png')}
+          resizeMode="contain"
+          className="ml-auto h-5 w-5 rotate-180"
+          tintColor={colors.primary[100]}
+        />
+      </View>
+      <View>
+        <TextBold className="mb-1 ml-5 text-h3">알람 설정</TextBold>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">PUSH 알림</TextRegular>
+          <Switch
+            trackColor={{
+              false: colors.gray[100],
+              true: colors.primary[100],
+            }}
+            thumbColor={colors.white}
+            ios_backgroundColor={colors.gray[100]}
+            onValueChange={() => {
+              setIsAlram(!isAlarm);
+            }}
+            value={isAlarm}
+            style={styles.switch}
+          />
+        </View>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">마케팅 정보 알림</TextRegular>
+          <Switch
+            trackColor={{
+              false: colors.gray[100],
+              true: colors.primary[100],
+            }}
+            thumbColor={colors.white}
+            ios_backgroundColor={colors.gray[100]}
+            onValueChange={() => {
+              setIsMarketing(!isMarketing);
+            }}
+            value={isMarketing}
+            style={styles.switch}
+          />
+        </View>
+      </View>
+      <View className="mt-5">
+        <TextBold className="mb-1 ml-5 text-h3">문의하기</TextBold>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">자주 묻는 질문</TextRegular>
+          <Image
+            source={require('@/assets/img/icon-arrow-left.png')}
+            resizeMode="contain"
+            className="ml-auto h-4 w-4 rotate-180"
+            tintColor={colors.gray[100]}
+          />
+        </View>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">1:1 문의하기</TextRegular>
+          <Image
+            source={require('@/assets/img/icon-arrow-left.png')}
+            resizeMode="contain"
+            className="ml-auto h-4 w-4 rotate-180"
+            tintColor={colors.gray[100]}
+          />
+        </View>
+      </View>
+      <View className="mt-5">
+        <TextBold className="mb-1 ml-5 text-h3">약관 및 정책</TextBold>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">이용 약관</TextRegular>
+          <Image
+            source={require('@/assets/img/icon-arrow-left.png')}
+            resizeMode="contain"
+            className="ml-auto h-4 w-4 rotate-180"
+            tintColor={colors.gray[100]}
+          />
+        </View>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">개인 정보 처리 방침</TextRegular>
+          <Image
+            source={require('@/assets/img/icon-arrow-left.png')}
+            resizeMode="contain"
+            className="ml-auto h-4 w-4 rotate-180"
+            tintColor={colors.gray[100]}
+          />
+        </View>
+        <View className="flex flex-row items-center border-b border-gray-25 px-5 py-3">
+          <TextRegular className="text-body1">위치 정보 이용 약관</TextRegular>
+          <Image
+            source={require('@/assets/img/icon-arrow-left.png')}
+            resizeMode="contain"
+            className="ml-auto h-4 w-4 rotate-180"
+            tintColor={colors.gray[100]}
+          />
+        </View>
+      </View>
+      <View className="ml-5 mt-5">
+        <TextMedium className="text-body2 text-gray-300">로그아웃</TextMedium>
+      </View>
+      <View className="ml-5 mt-5">
+        <TextMedium className="text-body2 text-gray-300">탈퇴하기</TextMedium>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  switch: {
+    transform: [{ scaleX: 0.7 }, { scaleY: 0.7 }],
+    marginLeft: 'auto',
+  },
+});
