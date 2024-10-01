cb67f2d503b7fae9140e5ba8cef72cb1ca50a0c6
timepresent95 <timepresent95@gmail.com>
2024-10-02 08:52:20 +0900

    feat: change MainHeader component
    #56

--- src/components/my/MainHeader.tsx
+++ src/components/my/MainHeader.tsx
@@ -4,7 +4,7 @@ import { Pressable, PressableProps, View } from 'react-native';
 import { TextBold } from '@/entities/fonts';
 
 interface Props {
-  title: string;
+  title?: string;
   left?: {
     onPress: PressableProps['onPress'];
     icon: ReactNode;
@@ -20,7 +20,7 @@ export default function MainHeader({ title, left, right }: Props) {
     <View className="relative h-14 py-4">
       {left && (
         <Pressable
-          className="absolute left-0 top-4 ml-4 h-5 w-5"
+          className="absolute left-4 top-1/2 z-10 h-5 w-5 translate-y-2"
           onPress={left.onPress}>
           {left.icon}
         </Pressable>
@@ -28,7 +28,7 @@ export default function MainHeader({ title, left, right }: Props) {
       <TextBold className="text-center text-h2">{title}</TextBold>
       {right && (
         <Pressable
-          className="absolute right-0 top-4 mr-4 h-5 w-5"
+          className="absolute right-4 top-1/2 z-10 h-5 w-5 translate-y-2 transform"
           onPress={right.onPress}>
           {right.icon}
         </Pressable>
