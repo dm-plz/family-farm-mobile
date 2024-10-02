c1f0dea950d1b7e43bdf62b4ddfa8924d826d8c7
timepresent95 <timepresent95@gmail.com>
2024-10-03 01:37:08 +0900

    feat: create launch screen storyboard for ios
    #62

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import ReactNativeModule, { NativeModules } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
 import queryClient from '@/api/queryClient';
 import GradientEndBackground from '@/entities/background/GradientEndBackground';
@@ -25,6 +26,12 @@ FirebaseMessagingModule.eventsAddListener('messaging_message_received');
 
 export default function App() {
   const [isMockingEnabled, setMockingEnabled] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000); //스플래시 활성화 시간
+  });
 
   useEffect(() => {
     const eventEmitter = new ReactNativeModule.NativeEventEmitter(
