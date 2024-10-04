956221b163011da55d11d4e0ca4d5be240db46f9
timepresent95 <timepresent95@gmail.com>
2024-10-04 17:47:01 +0900

    fix: remove not using component
    #62

--- src/pages/sign-up/Join3.tsx
+++ src/pages/sign-up/Join3.tsx
@@ -1,9 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
 
-import CustomButton from '@/components/CustomButton';
-import StepHeader from '@/components/sign-up/StepHeader';
 import { colors, routeNames } from '@/constants';
 import SelectableText from '@/entities/common/SelectableText';
 import { TextBold, TextRegular } from '@/entities/fonts';
@@ -17,8 +15,6 @@ type Join3ScreenProps = NativeStackScreenProps<
 >;
 
 function Join3({ navigation }: Join3ScreenProps) {
-  const isHost = false;
-
   const [selectedRole, setSelectedRole] = useState<string>('아빠');
   const [isFirstInFamily, setIsFirstInFamily] = useState<boolean>(false);
 
@@ -62,37 +58,6 @@ function Join3({ navigation }: Join3ScreenProps) {
           onPress={() => navigation.navigate(routeNames.JOIN4)}>
           <TextBold className="text-white text-h4">입력 완료</TextBold>
         </Pressable>
-      </View>
-      <View className="flex h-full flex-col justify-between px-10 pb-8">
-        <StepHeader currentStep={3} />
-        <View className="flex h-[40%] flex-col items-center justify-around">
-          <View className="bg-slate-200 flex h-20 w-60 items-center justify-center">
-            <Text className="text-center">로고</Text>
-          </View>
-          {isHost ? (
-            <>
-              <View>
-                <Text className="text-center font-bold">{`가은님,
-      가족에게 초대장 코드를 보내보세요!`}</Text>
-              </View>
-              <View className="bg-green-300 flex h-20 w-60 items-center justify-center">
-                <Text className="text-center">초대장 보내기</Text>
-              </View>
-              <View className="flex items-center justify-center">
-                <Text>or</Text>
-                <Text>나의 코드 복사</Text>
-                <Text className="text-xl font-bold underline">A2SDK3</Text>
-              </View>
-            </>
-          ) : (
-            <View className="items-center justify-center">
-              <Text className="font-bold">{`가은님,`}</Text>
-              <Text className="font-bold">{`가족과 소중한 추억 쌓으러 가볼까요?`}</Text>
-            </View>
-          )}
-        </View>
-        {/* TODO: Oauth 구현 이후, 이 시작하기 버튼을 누르면, UseAuth의 isLogin이 True로 바뀌어야 함. */}
-        <CustomButton>시작하기</CustomButton>
       </View>
     </SafeScreenWithHeader>
   );
