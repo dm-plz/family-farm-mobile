88048613a92890bfb9771fc6834843fb02aa3e5d
timepresent95 <timepresent95@gmail.com>
2024-10-07 00:35:35 +0900

    feat: enroll Descriptive Answer screen
    #55

--- src/navigations/stack/AnswerStackNavigator.tsx
+++ src/navigations/stack/AnswerStackNavigator.tsx
@@ -1,10 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { routeNames } from '@/constants';
+import DescriptiveAnswer from '@/pages/question-answer/DescriptiveAnswer';
 import RecordEmotion from '@/pages/question-answer/RecordEmotion';
 
 export type AnswerStackNavigatorParams = {
   [routeNames.RECORD_EMOTION]: undefined;
+  [routeNames.DESCRIPTIVE_ANSWER]: undefined;
 };
 
 const Stack = createStackNavigator<AnswerStackNavigatorParams>();
@@ -12,11 +14,15 @@ const Stack = createStackNavigator<AnswerStackNavigatorParams>();
 function AnswerStackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName={routeNames.RECORD_EMOTION}
+      initialRouteName={routeNames.DESCRIPTIVE_ANSWER}
       screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name={routeNames.RECORD_EMOTION}
         component={RecordEmotion}
+      />
+      <Stack.Screen
+        name={routeNames.DESCRIPTIVE_ANSWER}
+        component={DescriptiveAnswer}
       />
     </Stack.Navigator>
   );
