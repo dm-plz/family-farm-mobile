a8629930aaf47962088c84488c73cbf1e9e73d3d
timepresent95 <timepresent95@gmail.com>
2024-10-08 01:19:36 +0900

    chore: remove console.log
    #55

--- src/store/stores/backgroundStore.ts
+++ src/store/stores/backgroundStore.ts
@@ -13,19 +13,13 @@ type BackGroundStore = BackGroundStoreState & BackGroundStoreActions;
 const useBackGroundStore = create<BackGroundStore>(set => ({
   outOfSafeAreaBackgroundMode: 'default',
   setDefaultBackgroundMode: () =>
-    set(() => {
-      console.log('default');
-      return {
-        outOfSafeAreaBackgroundMode: 'default',
-      };
-    }),
+    set(() => ({
+      outOfSafeAreaBackgroundMode: 'default',
+    })),
   setGradientBackgroundMode: () =>
-    set(() => {
-      console.log('gradient');
-      return {
-        outOfSafeAreaBackgroundMode: 'gradient',
-      };
-    }),
+    set(() => ({
+      outOfSafeAreaBackgroundMode: 'gradient',
+    })),
 }));
 
 export default useBackGroundStore;
