f3585128603be956f3ccbf1c74ce02ab24b8ffc6
timepresent95 <timepresent95@gmail.com>
2024-10-14 14:05:33 +0900

    feat: modify toast util
    #65

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,17 +1,23 @@
-import Toast, { type ToastType } from 'react-native-toast-message';
+import Toast, {
+  type ToastProps,
+  type ToastType,
+} from 'react-native-toast-message';
 
 export type ToastContents = {
   title?: string;
   content?: string;
 };
+type ToastOptions = Omit<ToastProps, 'text1' | 'text2' | 'type'>;
 
 export function showToast(
   toastContents?: ToastContents,
   type: ToastType = 'info',
+  options?: ToastOptions,
 ) {
   Toast.show({
     type,
     text1: toastContents?.title,
     text2: toastContents?.content,
+    ...options,
   });
 }
